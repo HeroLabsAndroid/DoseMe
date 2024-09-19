a34287bcbf769d14a86f7d3b42379d3952431e1c
HeroLabs <jafrelange@gmail.com>
2024-09-19 12:10:49 +0200

    added exact alarms

--- app/src/main/java/com/example/doseme/notif/DoseMeAlarmScheduler.java
+++ app/src/main/java/com/example/doseme/notif/DoseMeAlarmScheduler.java
@@ -4,6 +4,7 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 
 import com.example.doseme.datadapt.MedAdapter;
@@ -28,13 +29,24 @@ public class DoseMeAlarmScheduler implements AlarmScheduler{
     public void schedule(AlarmItem item) {
         Intent intent = new Intent(ctx, AlarmReceiver.class).putExtra("XTRA_MSG", item.msg).putExtra("XTRA_CHNL", item.chnl).putExtra("XTRA_MEDID", item.medid);
 
-        alarmMan.setAndAllowWhileIdle(
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            if(alarmMan.canScheduleExactAlarms()) {
+                set_alarm(item, intent);
+            } else {
+                Log.e("ALARMSCHEDULE", "Not allowed to set exact notifs");
+            }
+        } else set_alarm(item, intent);
+
+
+    }
+
+    private void set_alarm(AlarmItem item, Intent intent) {
+        alarmMan.setExactAndAllowWhileIdle(
                 AlarmManager.RTC_WAKEUP,
                 item.timestamp.atZone(ZoneId.systemDefault()).toEpochSecond()*1000,
                 PendingIntent.getBroadcast(ctx, item.hashCode(), intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE)
         );
         Log.d("ALARMSCHEDULE", "Alarm set at "+ LocalDateTime.now().toString());
-
     }
 
     @Override
